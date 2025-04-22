832a47fd863dca1715b518b4ac1811f54cedc469
ebenois <ebenois@ensc.fr>
2025-04-22 11:32:19 +0200

    modification API

--- APItraitementnote.php
+++ APItraitementnote.php
@@ -1,23 +1,23 @@
 <?php
-    $host = 'localhost'; //variables de connexion
-    $dbname = 'gestion_notes';
-    $username = 'root';
-    $password = '';
-    try 
-    {
-        $bdd = new PDO('mysql:host='. $host .';dbname='. $dbname .';charset=utf8',
-        $username, $password);
-    } 
-    catch(Exception $e) 
-    {
-        // Si erreur, tout arrêter
-        die('Erreur : '. $e->getMessage());
-    }
-    $requete = 'SELECT * FROM utilisateurs';
-    $resultat = $bdd->query($requete);
-    //$tableau = $resultat->fetch(); // On récupère la première ligne
-    //echo $tableau["nom"] . " " . $tableau["prenom"];
-    while ($ligne = $resultat->fetch(PDO::FETCH_ASSOC)) {
-        echo $ligne["nom"] . " " . $ligne["prenom"] . "<br>";
-    }
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: POST, GET");
+header('Content-Type: application/json');
+$host = 'localhost';
+$dbname = 'gestion_notes';
+$username = 'root';
+$password = '';
+
+try {
+    $bdd = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $username, $password);
+} catch(Exception $e) {
+    die(json_encode(['error' => $e->getMessage()]));
+}
+
+$identifiant = $_GET['identifiant'] ?? '';
+$motDePasse = $_GET['motDePasse'] ?? '';
+$stmt = $bdd->prepare("SELECT * FROM notes WHERE id_eleve = (SELECT id FROM utilisateurs WHERE identifiant = ? AND mot_de_passe = ?)");
+$stmt->execute([$identifiant,$motDePasse]);
+$resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($resultats);
 ?>
