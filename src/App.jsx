b429e828f26400aa9c5c83c14f67fb4597af257b
ebenois <ebenois@ensc.fr>
2025-04-22 11:30:14 +0200

    Connexion entre REACT API BDD

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function Connexion() {
+  const [identifiant, setIdentifiant] = useState('');
+  const [motDePasse, setMotDePasse] = useState('');
+  const [message, setMessage] = useState('');
+  const [resultats, setResultats] = useState([]);
+
+  const handleConnexion = async (e) => {
+    e.preventDefault(); // empêche la soumission classique
+
+    try {
+      const response = await fetch(`http://localhost/APItraitementnote.php?identifiant=${identifiant}&motDePasse=${motDePasse}`);
+
+      const data = await response.json();
+
+      if (data.length > 0) {
+        setResultats(data)
+      } else {
+        setMessage("Identifiant ou mot de passe incorrect.");
+      }
+    } catch (error) {
+      setMessage("Erreur lors de la connexion.");
+    }
+  };
 
   return (
+    <div>
+      <div className="ENSC-image"></div>
+      <div className="container">
+        <div className='connexion'>
+          <h3 style={{ textAlign: "center" }}>Connexion</h3>
+          <form onSubmit={handleConnexion}>
+            <div>
+              <label htmlFor="identifiant">Identifiant</label>
+              <br />
+              <input
+                type="text"
+                id="identifiant"
+                value={identifiant}
+                onChange={(e) => setIdentifiant(e.target.value)}
+              />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="pass">Mot de passe</label>
+              <br />
+              <input
+                type="password"
+                id="pass"
+                value={motDePasse}
+                onChange={(e) => setMotDePasse(e.target.value)}
+                minLength="8"
+                required
+              />
+            </div>
+            <br />
+            <input type="submit" value="Se connecter" />
+          </form>
+          <div>
+          {resultats.map((note, index) => (
+          <div key={index}>
+            <p>Note : {note.note}</p>
+            <p>ID Matière : {note.id_matiere}</p>
+          </div>
+          ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Connexion />
     </>
   )
 }
-
-export default App
+export default App;
