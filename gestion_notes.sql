4a8924f3f267303c39d77b9c9ed5938123fea20e
matgarnierENSC <matgarnier@ensc.fr>
2025-04-22 11:14:32 +0200

    Update gestion_notes.sql

--- gestion_notes.sql
+++ gestion_notes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 22 avr. 2025 à 10:30
+-- Généré le : mar. 22 avr. 2025 à 11:14
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -37,7 +37,14 @@ CREATE TABLE `matieres` (
 --
 
 INSERT INTO `matieres` (`id`, `nom`) VALUES
-(1, 'Communication web');
+(1, 'Communication web'),
+(3, 'Programmation orientée objet'),
+(4, 'Cybersécurité'),
+(5, 'Design d’interaction'),
+(6, 'Gestion de projet'),
+(7, 'Bases de données'),
+(8, 'Systèmes embarqués'),
+(9, 'Psychologie cognitive');
 
 -- --------------------------------------------------------
 
@@ -53,6 +60,27 @@ CREATE TABLE `notes` (
   `note` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `notes`
+--
+
+INSERT INTO `notes` (`id`, `id_eleve`, `id_prof`, `id_matiere`, `note`) VALUES
+(1, 4, 1, 1, 16.5),
+(2, 5, 1, 1, 14),
+(3, 6, 1, 1, 15.5),
+(4, 7, 1, 1, 17),
+(5, 8, 1, 1, 13.5),
+(10, 4, 3, 6, 11),
+(11, 5, 3, 6, 13),
+(12, 6, 3, 6, 14),
+(13, 9, 3, 6, 15.5),
+(14, 9, 1, 4, 16),
+(15, 8, 1, 4, 15),
+(16, 7, 1, 4, 14),
+(17, 4, 2, 3, 12.5),
+(18, 5, 2, 3, 13),
+(19, 6, 2, 3, 15.5);
+
 -- --------------------------------------------------------
 
 --
@@ -63,7 +91,7 @@ CREATE TABLE `utilisateurs` (
   `id` int(11) NOT NULL,
   `nom` varchar(100) DEFAULT NULL,
   `prenom` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
+  `identifiant` varchar(100) DEFAULT NULL,
   `mot_de_passe` varchar(255) DEFAULT NULL,
   `role` enum('eleve','prof') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -72,10 +100,16 @@ CREATE TABLE `utilisateurs` (
 -- Déchargement des données de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `mot_de_passe`, `role`) VALUES
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `identifiant`, `mot_de_passe`, `role`) VALUES
 (1, 'GARNIER', 'Mathis', 'matgarnier@ensc.fr', 'christophe4ever', 'eleve'),
 (2, 'BENOIS', 'Elian', 'ebenois@ensc.fr', 'ilovekiki', 'eleve'),
-(3, 'PLACIN', 'Frederique', 'freplacin@ensc.fr', 'champi4ever', 'prof');
+(3, 'PLACIN', 'Frederique', 'freplacin@ensc.fr', 'champi4ever', 'prof'),
+(4, 'VERDIER', 'Julien', 'jverdier@ensc.fr', 'promaster', 'prof'),
+(5, 'DUPONT', 'Claire', 'cdupont@ensc.fr', 'dataqueen', 'prof'),
+(6, 'KABORE', 'Aïcha', 'akabore@ensc.fr', 'securepass123', 'eleve'),
+(7, 'LAMBERT', 'Sophie', 'slambert@ensc.fr', 'soso2025', 'eleve'),
+(8, 'TOUZET', 'Lucas', 'ltouzet@ensc.fr', 'lucasnotes', 'eleve'),
+(9, 'MARTIN', 'Nathan', 'nmartin@ensc.fr', 'nathan456', 'eleve');
 
 --
 -- Index pour les tables déchargées
@@ -110,19 +144,19 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `matieres`
 --
 ALTER TABLE `matieres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Contraintes pour les tables déchargées
