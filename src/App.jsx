e56cf887f8095b7890132b898974cb51008ee952
ebenois <ebenois@ensc.fr>
2025-05-08 11:12:15 +0200

    Ajout Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -94,34 +94,33 @@ function Connexion() {
 
   // Sinon, afficher le formulaire
   return (
-    <div>
-      <h3 style={{ textAlign: "center" }}>Connexion</h3>
-      <form onSubmit={handleConnexion}>
-        <div>
-          <label htmlFor="identifiant">Identifiant</label><br />
-          <input
-            type="text"
-            id="identifiant"
-            value={identifiant}
-            onChange={(e) => setIdentifiant(e.target.value)}
-          />
+    <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div class="max-w-md mx-auto">
+            <div>
+              <h1 class="text-2xl font-semibold">Connexion</h1>
+            </div>
+            <form onSubmit={handleConnexion} class="divide-y divide-gray-200">
+              <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <div class="relative">
+                  <input onChange={(e) => setIdentifiant(e.target.value)} required value={identifiant} autocomplete="off" id="identifiant" name="identifiant" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="identifiant" />
+                  <label for="identifiant" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Identifiant</label>
+                </div>
+                <div class="relative">
+                  <input onChange={(e) => setMotDePasse(e.target.value)} minLength="8" required value={motDePasse} autocomplete="off" id="pass" name="pass" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="pass" />
+                  <label for="pass" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Mot de passe</label>
+                </div>
+                <div class="relative">
+                  <input type="submit" value="Se connecter" class="bg-blue-500 text-white rounded-md px-2 py-1"/>
+                </div>
+              </div>
+            </form>
+            {message && <p style={{ color: 'red' }}>{message}</p>}
+          </div>
         </div>
-        <br />
-        <div>
-          <label htmlFor="pass">Mot de passe</label><br />
-          <input
-            type="password"
-            id="pass"
-            value={motDePasse}
-            onChange={(e) => setMotDePasse(e.target.value)}
-            minLength="8"
-            required
-          />
-        </div>
-        <br />
-        <input type="submit" value="Se connecter" />
-      </form>
-      {message && <p style={{ color: 'red' }}>{message}</p>}
+      </div>
     </div>
   );
 }
@@ -243,13 +242,7 @@ function Notes() {
 
 function App() {
   return (
-    <div>
-      <div className="container">
-        <div className='connexion'>
-          <Connexion />
-        </div>
-      </div>
-    </div>
+    <Connexion />
   );
 }
 
