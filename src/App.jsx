4d3ad1ab0893893c7c68c3fed138849149e00f84
ebenois <ebenois@ensc.fr>
2025-05-13 17:22:25 +0200

    Modification ajout notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function Connexion() {
@@ -13,18 +13,17 @@ function Connexion() {
     e.preventDefault();
 
     try {
-      const response = await fetch(`https://ebenois.zzz.bordeaux-inp.fr/APItraitementnote.php?identifiant=${identifiant}&motDePasse=${motDePasse}`);
+      const response = await fetch(`https://ebenois.zzz.bordeaux-inp.fr/APItraitementnote.php?identifiant=${encodeURIComponent(identifiant)}&motDePasse=${encodeURIComponent(motDePasse)}`);
       const data = await response.json();
 
       if (data.length > 0) {
         setResultats(data);
         setMessage('');
         setEstConnecte(true);
-        let total = 0;
-        data.forEach(element => {
-          total += parseFloat(element.note);
-        });
-        setMoyenne((total / data.length).toFixed(2));
+
+        const notes = data.filter(d => d.job === "eleve");
+        const total = notes.reduce((acc, curr) => acc + parseFloat(curr.note), 0);
+        setMoyenne((total / notes.length).toFixed(2));
       } else {
         setMessage("Identifiant ou mot de passe incorrect.");
       }
@@ -33,65 +32,62 @@ function Connexion() {
     }
   };
 
-
   if (estConnecte) {
     return (
-      <div class="text-gray-900 bg-gray-200">
-        <div class="p-4 flex">
-          <h1 class="text-3xl">Récapitulatif</h1>
+      <div className="text-gray-900 bg-gray-200">
+        <div className="p-4 flex">
+          <h1 className="text-3xl">Récapitulatif</h1>
         </div>
-        <div class="px-3 py-4">
-          
-            {resultats.some(r => r.job === "eleve") && (
-              <table class="w-full text-md bg-white shadow-md rounded mb-4">
-                <tbody>
-                  <tr class="border-b">
-                    <th class="text-left p-3 px-5">Matières</th>
-                    <th class="text-left p-3 px-5">Notes</th>
+        <div className="px-3 py-4">
+          {resultats.some(r => r.job === "eleve") && (
+            <table className="w-full text-md bg-white shadow-md rounded mb-4">
+              <tbody>
+                <tr className="border-b">
+                  <th className="text-left p-3 px-5">Matières</th>
+                  <th className="text-left p-3 px-5">Notes</th>
+                </tr>
+                {resultats.map((note, index) => (
+                  <tr className="border-b hover:bg-orange-100 bg-gray-100" key={index}>
+                    <td className="p-3 px-5">{note.nom_matiere}</td>
+                    <td className="p-3 px-5">{note.note}</td>
                   </tr>
-                  {resultats.map((note, index) => (
-                    <tr class="border-b hover:bg-orange-100 bg-gray-100" key={index}>
-                      <td class="p-3 px-5">{note.nom_matiere}</td>
-                      <td class="p-3 px-5">{note.note}</td>
-                    </tr>
-                  ))}
-                  <tr class="border-b hover:bg-orange-100 bg-gray-100">
-                    <td class="p-3 px-5 "><b>Moyenne</b></td>
-                    <td class="p-3 px-5"><b>{moyenne}</b></td>
-                  </tr>
-                </tbody>
-              </table>
-            )}
-            {resultats.some(r => r.job === "prof") && (
-              <Recapitulatif/>
-            )}
+                ))}
+                <tr className="border-b hover:bg-orange-100 bg-gray-100">
+                  <td className="p-3 px-5"><b>Moyenne</b></td>
+                  <td className="p-3 px-5"><b>{moyenne}</b></td>
+                </tr>
+              </tbody>
+            </table>
+          )}
+          {resultats.some(r => r.job === "prof") && (
+            <Recapitulatif />
+          )}
         </div>
       </div>
     );
   }
 
-  // Sinon, afficher le formulaire
   return (
-    <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <div class="max-w-md mx-auto">
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="max-w-md mx-auto">
             <div>
-              <h1 class="text-2xl font-semibold">Connexion</h1>
+              <h1 className="text-2xl font-semibold">Connexion</h1>
             </div>
-            <form onSubmit={handleConnexion} class="divide-y divide-gray-200">
-              <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <div class="relative">
-                  <input onChange={(e) => setIdentifiant(e.target.value)} required value={identifiant} autocomplete="off" id="identifiant" name="identifiant" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="identifiant" />
-                  <label for="identifiant" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Identifiant</label>
+            <form onSubmit={handleConnexion} className="divide-y divide-gray-200">
+              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <div className="relative">
+                  <input onChange={(e) => setIdentifiant(e.target.value)} required value={identifiant} autoComplete="off" id="identifiant" name="identifiant" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none" placeholder="identifiant" />
+                  <label htmlFor="identifiant" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Identifiant</label>
                 </div>
-                <div class="relative">
-                  <input onChange={(e) => setMotDePasse(e.target.value)} minLength="8" required value={motDePasse} autocomplete="off" id="pass" name="pass" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="pass" />
-                  <label for="pass" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Mot de passe</label>
+                <div className="relative">
+                  <input onChange={(e) => setMotDePasse(e.target.value)} minLength="8" required value={motDePasse} autoComplete="off" id="pass" name="pass" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none" placeholder="pass" />
+                  <label htmlFor="pass" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Mot de passe</label>
                 </div>
-                <div class="relative">
-                  <input type="submit" value="Se connecter" class="bg-blue-500 text-white rounded-md px-2 py-1"/>
+                <div className="relative">
+                  <input type="submit" value="Se connecter" className="bg-blue-500 text-white rounded-md px-2 py-1" />
                 </div>
               </div>
             </form>
@@ -103,21 +99,24 @@ function Connexion() {
   );
 }
 
-
 function Recapitulatif() {
   const [recap, setRecap] = useState([]);
+  const nextId = useRef(0);
+
   const ajouterNote = () => {
     const nouvelleNote = {
-      id: nextId++,
-      matiere: '',
-      eleve: '',
+      id: nextId.current++,
+      nom_matiere: '',
+      nom_eleve: '',
       note: 0,
     };
     setRecap([...recap, nouvelleNote]);
   };
+
   const SupprimerNote = (idASupprimer) => {
     setRecap(recap.filter((note) => note.id !== idASupprimer));
   };
+
   const mettreAJourNote = (id, nouvelleValeur) => {
     setRecap(recap.map(p => (p.id === id ? nouvelleValeur : p)));
   };
@@ -142,20 +141,20 @@ function Recapitulatif() {
     } catch (erreur) {
     console.error('Erreur lors de l\'envoi des données :', erreur);
     }*/
-    };
+  };
 
   return (
     <>
-      <table class="w-full text-md bg-white shadow-md rounded mb-4">
+      <table className="w-full text-md bg-white shadow-md rounded mb-4">
         <tbody>
-          <tr class="border-b">
-            <th class="text-left p-3 px-5">Matières</th>
-            <th class="text-left p-3 px-5">Nom</th>
-            <th class="text-left p-3 px-5">Notes</th>
+          <tr className="border-b">
+            <th className="text-left p-3 px-5">Matières</th>
+            <th className="text-left p-3 px-5">Nom</th>
+            <th className="text-left p-3 px-5">Notes</th>
             <th></th>
           </tr>
           {recap.map((note) => (
-            <tr class="border-b hover:bg-orange-100 bg-gray-100" key={note.id}>
+            <tr className="border-b hover:bg-orange-100 bg-gray-100" key={note.id}>
               <Note
                 id={note.id}
                 data={note}
@@ -164,70 +163,71 @@ function Recapitulatif() {
               />
             </tr>
           ))}
-      </tbody>
-    </table>
-    <div class="flex">
-      <button class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline" type="button" onClick={ajouterNote}>Ajouter une nouvelle note</button>
-      <button class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline" type="button" onClick={soumission}>Envoyer</button>
-    </div>
-  </>
+        </tbody>
+      </table>
+      <div className="flex">
+        <button className="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded" type="button" onClick={ajouterNote}>Ajouter une nouvelle note</button>
+        <button className="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded" type="button" onClick={soumission}>Envoyer</button>
+      </div>
+    </>
   );
 }
 
-//dump
-const nextId = useRef(0);
-
 function Note({ id, data, onChange, Supprimer }) {
   const [modification, setModification] = useState(false);
-  const modifier = () => setModification(!modification);
+  const toggleModification = () => setModification(!modification);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     onChange(id, { ...data, [name]: name === "note" ? parseFloat(value) || 0 : value });
   };
+
   return (
     <>
-      <td class="p-3 px-5">
+      <td className="p-3 px-5">
         <input
           list="matiere"
           type="text"
-          name="matiere"
-          value={data.matiere}
+          name="nom_matiere"
+          value={data.nom_matiere}
           onChange={handleChange}
-          disabled={modification}
+          disabled={!modification}
         />
         <datalist id="matiere">
           <option value="Maths" />
           <option value="Physique" />
         </datalist>
       </td>
-      <td class="p-3 px-5">
+      <td className="p-3 px-5">
         <input
-            list="eleve"
-            type="text"
-            name="eleve"
-            value={data.eleve}
-            onChange={handleChange}
-            disabled={modification}
-          />
+          list="eleve"
+          type="text"
+          name="nom_eleve"
+          value={data.nom_eleve}
+          onChange={handleChange}
+          disabled={!modification}
+        />
         <datalist id="eleve">
           <option value="GARNIER Mathis" />
           <option value="BENOIS Elian" />
         </datalist>
       </td>
-      <td class="p-3 px-5">
+      <td className="p-3 px-5">
         <input
           type="number"
           name="note"
           value={data.note}
           onChange={handleChange}
-          disabled={modification}
-          min="0" 
+          disabled={!modification}
+          min="0"
           max="20"
         />
       </td>
-      <td class="p-3 px-5 flex justify-end">
-        <button class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline" type="button" onClick={modifier}>{modification ? 'modifier' : 'enregistrer'}</button>
-        <button class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => Supprimer(id)}>Supprimer</button>
+      <td className="p-3 px-5 flex justify-end">
+        <button className="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded" type="button" onClick={toggleModification}>
+          {modification ? 'Enregistrer' : 'Modifier'}
+        </button>
+        <button className="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded" type="button" onClick={() => Supprimer(id)}>Supprimer</button>
       </td>
     </>
   );
